eafb7501b0856b7eba20cf49698e24df08dfc2ec
QATester22-25 <hednaili@gmail.com>
2026-05-05 18:15:15 +0100

    corect path

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import tick from '/To-Do-List/docs/assets/tick-kJw2yUx5.png'
-import untick from '/To-Do-List/docs/assets/untick-CI1pMQDd.png'
-import delete_icon from '/To-Do-List/delete.png'
+import tick from '../assets/tick.png'
+import untick from '../assets/untick.png'
+import delete_icon from '../assets/delete.png'
 
 const TodoItems = ({ text,id,isComplete,deleteTodo,toggle}) => {
   return (
