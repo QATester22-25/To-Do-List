d4a2d8da2d1f11cbbe9ecdecf65d29f2714dd29a
QATester22-25 <hednaili@gmail.com>
2026-05-06 22:44:34 +0100

    correct ui test assertion

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -5,10 +5,10 @@ import delete_icon from '../assets/delete.png'
 
 const TodoItems = ({ text,id,isComplete,deleteTodo,toggle}) => {
   return (
-    <div className='flex items-center my-3 gap-2 '>
+    <div className='flex items-center my-3 gap-2'>
       <div  className='flex flex-1 items-center cursor-pointer'>
         <img onClick={()=>{toggle(id)}} className='w-7' src={isComplete ? tick : untick} alt="" />
-        <p className={`text-slate-700 ml-4 text-[25px] ${isComplete ? 'text-red-700 font-bold underline' : ''}`}>{text}</p>
+        <p className={`text-slate-700 ml-4 text-[25px] ${isComplete ? 'font-bold underline' : ''}`}>{text}</p>
       </div>
       <img onClick={() => deleteTodo(id)} src={delete_icon} alt="delete" className='w-5.5 cursor-pointer' />
     </div>
