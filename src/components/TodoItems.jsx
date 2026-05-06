c1ba128c782fa6986348ef1528f1066b972d0e49
QATester22-25 <hednaili@gmail.com>
2026-05-06 22:53:44 +0100

    Add onclik event on icon

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -7,7 +7,7 @@ const TodoItems = ({ text,id,isComplete,deleteTodo,toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
       <div  className='flex flex-1 items-center cursor-pointer'>
-        <img  className='w-7' src={isComplete ? tick : untick} alt="" />
+        <img onClick={()=>{toggle(id)}} className='w-7' src={isComplete ? tick : untick} alt="" />
         <p onClick={()=>{toggle(id)}} className={`text-slate-700 ml-4 text-[25px] ${isComplete ? 'font-bold underline' : ''}`}>{text}</p>
       </div>
       <img onClick={() => deleteTodo(id)} src={delete_icon} alt="delete" className='w-5.5 cursor-pointer' />
