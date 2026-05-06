455b590b421154e85e7277807586abdcf19d8476
QATester22-25 <hednaili@gmail.com>
2026-05-06 22:48:57 +0100

    change onclick event on text

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -7,8 +7,8 @@ const TodoItems = ({ text,id,isComplete,deleteTodo,toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
       <div  className='flex flex-1 items-center cursor-pointer'>
-        <img onClick={()=>{toggle(id)}} className='w-7' src={isComplete ? tick : untick} alt="" />
-        <p className={`text-slate-700 ml-4 text-[25px] ${isComplete ? 'font-bold underline' : ''}`}>{text}</p>
+        <img  className='w-7' src={isComplete ? tick : untick} alt="" />
+        <p onClick={()=>{toggle(id)}} className={`text-slate-700 ml-4 text-[25px] ${isComplete ? 'font-bold underline' : ''}`}>{text}</p>
       </div>
       <img onClick={() => deleteTodo(id)} src={delete_icon} alt="delete" className='w-5.5 cursor-pointer' />
     </div>
