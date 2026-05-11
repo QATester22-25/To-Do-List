1d02f8cdb25d428dbf47e0a40d6f58991f9a2116
QATester22-25 <hednaili@gmail.com>
2026-05-11 18:40:00 +0100

    Add filter buttons

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -67,9 +67,15 @@ const Todo = () => {
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-137.5 rounded-xl">
       {/* ----------title------------ */}
 
-      <div className="flex items-center mt-7 gap-2">
+      <div className="flex items-center my-2 gap-2">
         <img className="w-8" src={to_do_list} alt="" />
         <h1 className="text-3xl font-semibold">To-Do List</h1>
+      </div>
+      {/*filter*/}
+      <div className="flex justify-evenly mt-4 text-xl">
+        <button className="font-bold cursor-pointer text-emerald-800 border hover:bg-emerald-900 hover:text-white px-4 py-2 rounded-xl">All</button>
+        <button className="font-bold cursor-pointer text-emerald-800 border hover:bg-emerald-900 hover:text-white px-4 py-2 rounded-xl">Remaining</button>
+        <button className="font-bold cursor-pointer text-emerald-800 border hover:bg-emerald-900 hover:text-white px-4 py-2 rounded-xl" >Done</button>
       </div>
 
       {/* ----------input box------------ */}
@@ -78,7 +84,7 @@ const Todo = () => {
         <input
           ref={inputRef}
           onFocus={() => setError("")}
-          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600 mt-2"
           type="text"
           placeholder="Add your task"
         />
